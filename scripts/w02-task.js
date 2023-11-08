e9143b4c752f777be1942f6f7fc10fb62cd43838
Jared Morillo <jared.morillo@hotmail.com>
2023-11-08 14:09:43 -0500

    Task02

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,25 +3,42 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
+const fullName = 'Jared Morillo';
+const currentYear = new Date().getFullYear();
+const profilePicture = 'images/JG_After.jpg';
 
 
 /* Step 3 - Element Variables */
-
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
 
 
 
 /* Step 4 - Adding Content */
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 
 
 
 
 /* Step 5 - Array */
+const favFoods = ['Pizza', 'Sushi', 'Chocolate', 'Ice Cream'];
+foodElement.innerHTML = favFoods.join('<br>');
 
+const newFavoriteFood = 'Burgers';
+favFoods.push(newFavoriteFood);
+foodElement.innerHTML += `<br>${newFavoriteFood}`;
+
+favFoods.shift();
+foodElement.innerHTML = favFoods.join('<br>');
+
+favFoods.pop();
+foodElement.innerHTML = favFoods.join('<br>');
 
 
 
